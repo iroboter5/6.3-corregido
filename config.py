8ec9f31d28db706a1909b75eead88195878309a7
Devin <100889237+iroboter5@users.noreply.github.com>
2022-12-29 13:46:57 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ.get('5313616769:AAEVpjsls8lVQsnHIfbJTFFmWDESh94LEpY','')
+BOT_TOKEN = os.environ.get('5983160701:AAGR4kw8LcYuPVtp9JdXTmB6whQPM8-AtO8','')
 TG_API_ID = os.environ.get('14681595',0)
 TG_API_HASH = os.environ.get('a86730aab5c59953c424abb4396d32d5','')
 TG_ADMIN_USER = os.environ.get('nautai','ana_laura01')
